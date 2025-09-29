490316ac7ff4f5b84f487339aebd9b252009faa3
Fedello995 <103048732+Fedello995@users.noreply.github.com>
2025-09-29 13:23:19 +0200

    Delete utils directory

--- utils/colores.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class Colores {
-    public static final String RESET = "\u001B[0m";
-    public static final String ROJO = "\u001B[31m";
-    public static final String VERDE = "\u001B[32m";
-    public static final String AZUL = "\u001B[34m";
-}
-
