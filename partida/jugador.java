d345598d21aa8ed26a37da25aac30eaef0174557
fosaseptica <javiermartinhc@gmail.com>
2025-09-29 13:23:58 +0200

    Add files via upload

---
+++ partida/jugador.java
@@ -0,0 +1,15 @@
+public package partida;
+
+public class Jugador {
+    private String nombre;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+} {
+    
+}
