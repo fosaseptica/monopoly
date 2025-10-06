c197a0ded40d2ac0b5d6f2652649989a5eb0a5c6
fosaseptica <javiermartinhc@gmail.com>
2025-10-06 12:40:24 +0200

    Update Grupo.java

--- monopoly/Grupo.java
+++ monopoly/Grupo.java
@@ -13,24 +13,40 @@ class Grupo {
 
     //Constructor vacío.
     public Grupo() {
+        miembros = new ArrayList<>();
+        colorGrupo = "";
+        numCasillas = 0;
     }
 
     /*Constructor para cuando el grupo está formado por DOS CASILLAS:
     * Requiere como parámetros las dos casillas miembro y el color del grupo.
      */
     public Grupo(Casilla cas1, Casilla cas2, String colorGrupo) {
+        miembros = new ArrayList<>();
+        miembros.add(cas1);
+        miembros.add(cas2);
+        this.colorGrupo = colorGrupo;
+        numCasillas = 2;
     }
 
     /*Constructor para cuando el grupo está formado por TRES CASILLAS:
     * Requiere como parámetros las tres casillas miembro y el color del grupo.
      */
     public Grupo(Casilla cas1, Casilla cas2, Casilla cas3, String colorGrupo) {
+        miembros = new ArrayList<>();
+        miembros.add(cas1);
+        miembros.add(cas2);
+        miembros.add(cas3);
+        this.colorGrupo = colorGrupo;
+        numCasillas = 3;
     }
 
     /* Método que añade una casilla al array de casillas miembro de un grupo.
     * Parámetro: casilla que se quiere añadir.
      */
     public void anhadirCasilla(Casilla miembro) {
+        miembros.add(miembro);
+        numCasillas++;
     }
 
     /*Método que comprueba si el jugador pasado tiene en su haber todas las casillas del grupo:
@@ -38,7 +54,20 @@ class Grupo {
     * Valor devuelto: true si es dueño de todas las casillas del grupo, false en otro caso.
      */
     public boolean esDuenhoGrupo(Jugador jugador) {
+        for(Casilla c : miembros) {
+            if(c.getDuenho() != jugador) {
+                return false;//alguna casilla no es del jugador
+            }
+        }
+    return true;// todas las casillas son del jugador
     }
+
+    /*necesario esto en casilla
+    // Getter para obtener el dueño de la casilla
+    public Jugador getDuenho() {
+        return duenho;
+    }
+    */
 
 }
 
