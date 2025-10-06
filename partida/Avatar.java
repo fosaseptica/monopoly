9368de7be78f5d408c466b3dca190350103ddb59
ineelande <ineelanderodr@gmail.com>
2025-10-06 12:40:17 +0200

    Avatar.java

--- partida/Avatar.java
+++ partida/Avatar.java
@@ -3,6 +3,7 @@ package partida;
 import monopoly.*;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Avatar {
@@ -22,6 +23,13 @@ public class Avatar {
     * avatares creados (usado para crear un ID distinto del de los demás avatares).
      */
     public Avatar(String tipo, Jugador jugador, Casilla lugar, ArrayList<Avatar> avCreados) {
+        this.tipo=tipo;
+        this.jugador=jugador;
+        this.lugar=lugar;
+        generarId(avCreados);
+        avCreados.add(this);
+        lugar.anhadirAvatar(this);
+        
     }
 
     //A continuación, tenemos otros métodos útiles para el desarrollo del juego.
@@ -31,6 +39,28 @@ public class Avatar {
     * EN ESTA VERSIÓN SUPONEMOS QUE valorTirada siemrpe es positivo.
      */
     public void moverAvatar(ArrayList<ArrayList<Casilla>> casillas, int valorTirada) {
+        int posActual = lugar.getPosicion() - 1; 
+        int nuevaPos = (posActual + valorTirada) % 40;  // 40 casillas
+
+        // Determinar lado e índice dentro del lado
+        int lado, index;
+        if (nuevaPos < 11) {            // lado superior
+            lado = 0;
+            index = nuevaPos;
+        } else if (nuevaPos < 20) {     // lado izquierdo
+            lado = 1;
+            index = nuevaPos - 11;
+        } else if (nuevaPos < 31) {     // lado inferior
+            lado = 2;
+            index = nuevaPos - 20;
+        } else {                        // lado derecho
+            lado = 3;
+            index = nuevaPos - 31;
+        }
+
+        // Asignar la nueva casilla
+        this.lugar = casillas.get(lado).get(index);
+        lugar.anhadirAvatar(this);
     }
 
     /*Método que permite generar un ID para un avatar. Sólo lo usamos en esta clase (por ello es privado).
@@ -38,6 +68,47 @@ public class Avatar {
     * - Un arraylist de los avatares ya creados, con el objetivo de evitar que se generen dos ID iguales.
      */
     private void generarId(ArrayList<Avatar> avCreados) {
-    }
-}
+        Random rand = new Random();
+        String candidato;
+        boolean repetido;
 
+        do {
+            candidato = String.valueOf((char) ('A' + rand.nextInt(26)));
+            repetido = false;
+            for (Avatar a : avCreados) {
+                if (a.getId().equals(candidato)) {
+                    repetido = true;
+                    break;
+                }
+            }
+        } while (repetido);
+
+        this.id = candidato;
+    }
+
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public Casilla getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(Casilla lugar) {
+        this.lugar = lugar;
+    }
+    @Override
+    public String toString() {
+        return "Avatar " + id + " (" + tipo + ")";
+    }
+
+}
