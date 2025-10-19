aee200cd0a7c03362384b5ee1df7fb9cdb53d878
Fedello995 <103048732+Fedello995@users.noreply.github.com>
2025-10-19 22:13:59 +0200

    Update MonopolyETSE.java

--- modificaciones 1entrega/monopoly/MonopolyETSE.java
+++ modificaciones 1entrega/monopoly/MonopolyETSE.java
@@ -1,13 +1,9 @@
 package monopoly;
 
 public class MonopolyETSE {
+
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        
-        if (args.length > 0) {
-            menu.leerComandosDesdeFichero(args[0]);
-        }
-        
-        menu.iniciarPartidaPublico();  // ← Iniciar partida DESPUÉS
+        new Menu();
     }
+
 }
